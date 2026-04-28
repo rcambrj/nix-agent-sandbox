c6cf35033cbdcadfd34ceae0ce12259cc0f46571
Robert Cambridge <robert@cambridge.me>
2026-04-28 21:27:58 +0200

    add comments

--- lib/default.nix
+++ lib/default.nix
@@ -3,26 +3,28 @@
 let
   inherit (inputs.nixpkgs) lib;
 
-  optionalFlag = import ./optional-flag.nix {
-    inherit lib;
-  };
-
-  normalizeExtraModules = import ./normalize-extra-modules.nix { };
-
-  renderExtraFlags = import ./render-extra-flags.nix {
-    inherit lib;
-  };
-
+  # Build the host-side launcher script used to boot and connect to the VM.
   mkLauncherScript = import ./mk-launcher-script.nix {
     inherit lib renderExtraFlags;
   };
 
+  # Build an executable sandbox package with a guest VM and launcher.
   mkSandboxPackage = import ./mk-sandbox-package.nix {
     inherit flake inputs normalizeExtraModules;
   };
 
+  # Wrap a sandbox package with default launcher flags.
   mkWrappedExec = import ./mk-wrapped-exec.nix {
     inherit lib optionalFlag;
+  };
+
+  # Internal helpers
+  normalizeExtraModules = import ./normalize-extra-modules.nix { };
+  renderExtraFlags = import ./render-extra-flags.nix {
+    inherit lib;
+  };
+  optionalFlag = import ./optional-flag.nix {
+    inherit lib;
   };
 in
 {
